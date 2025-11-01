4c83ce97bb1f4b96acd9a3fbea5e7283f517cd19
Romeiro <theromeirofernandes@gmail.com>
2025-11-01 18:12:03 +0530

    added requirements.txt

--- ml/main.py
+++ ml/main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 # CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "https://gamblr.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
