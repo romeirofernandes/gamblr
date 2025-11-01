866de0ff311435eb43ff3b96a84e35e3a8c87893
Romeiro <theromeirofernandes@gmail.com>
2025-11-01 09:33:32 +0530

    preprocessing done

---
+++ ml/scripts/preprocessing.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import re
+
+df = pd.read_csv('../dataset.csv')
+
+# Fixing date formats to DD/MM/YYYY
+def fix_date(date_str):
+    match = re.match(r'(\d{2})/(\d{2})/(\d{2,4})', str(date_str))
+    if match:
+        day, month, year = match.groups()
+        if len(year) == 2:
+            year = '20' + year if int(year) < 50 else '19' + year
+        return f"{day}/{month}/{year}"
+    else:
+        return date_str 
+
+df['Date'] = df['Date'].apply(fix_date)
+
+# Replacing 'NH' with 'A' in the FTR column for old records
+df['FTR'] = df['FTR'].replace('NH', 'A')
+
+#Setting FTR based on FTHG and FTAG values
+df.loc[df['FTHG'] > df['FTAG'], 'FTR'] = 'H'
+df.loc[df['FTHG'] < df['FTAG'], 'FTR'] = 'A'
+df.loc[df['FTHG'] == df['FTAG'], 'FTR'] = 'D'
+
+df.to_csv('dataset.csv', index=False)
